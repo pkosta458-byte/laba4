0f775e2ac2e6d48f0980cacc5a1dedf7d6c9dc71
pkosta458-byte <pkosta458@gmail.com>
2026-06-19 11:15:27 +0500

    Add files via upload

---
+++ laba4/laba4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[] numbers = new int[15];
+            int count = 0;
+            Console.WriteLine("Сгенерированные числа:");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = rnd.Next(10, 100);
+                Console.Write(numbers[i] + " ");
+            }
+            Console.WriteLine();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int firstDigit = numbers[i] / 10;
+                int secondDigit = numbers[i] % 10; 
+                if (firstDigit <= secondDigit)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine("\nКоличество чисел, где первая цифра больше второй: " + count);
+            Console.ReadKey();
+        }
+    }
+}
